8cd53bafb94141ef1c7a0969e3aad6712c8588bd
PolloLopez <pollolopeza@gmail.com>
2024-05-21 14:44:51 -0300

    falta hacer el carrito

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 // ir a cada seccion
 // Obtener el enlace "Nosotros" y la sección "sobreNosotros"
-const nosotrosLink = document.querySelector('a[href="nosotros"] ');
-const nosotrosSection = document.querySelector('.sobreNosotros');
+//const nosotrosLink = document.querySelector('a[href="nosotros"] ');
+//const nosotrosSection = document.querySelector('.sobreNosotros');
 
 // Agregar evento de clic al enlace
-nosotrosLink.addEventListener('click', function (event) {
-    // Prevenir el comportamiento predeterminado del enlace
-    event.preventDefault();
+//nosotrosLink.addEventListener('click', function (event) {
+//    // Prevenir el comportamiento predeterminado del enlace
+//    event.preventDefault();
 
     // Hacer scroll suave hacia la sección "sobreNosotros"
-    nosotrosSection.scrollIntoView({ behavior: 'smooth' });
+   // nosotrosSection.scrollIntoView({ behavior: 'smooth' });
+//});
+
+
+// Obtener todos los enlaces que apuntan a secciones dentro de la página
+const navLinks = document.querySelectorAll('a[href^="#"]');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
 });
