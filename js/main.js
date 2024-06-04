414fc367eb50a47907369ce6c5775a5ffb0e8ae2
PolloLopez <pollolopeza@gmail.com>
2024-06-04 14:20:35 -0300

    falta carrito y sus funciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+/*** DIRIGE AL PRODUCTO ***/
 const navLinks = document.querySelectorAll('a[href^="#"]');// Obtener todos los enlaces que apuntan a secciones dentro de la página
+
 
 navLinks.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -30,6 +32,7 @@ const categorias = [
     { nombre: "POSTRES", contenedor: "#contPostres" }
 ];
 
+
 /*traer de base de datos local */
 fetch("./data/productos.json")
     .then(res => res.json())    
@@ -45,9 +48,10 @@ fetch("./data/productos.json")
     });
 
 
-
 // Función para mostrar los productos en el contenedor especificado
 const mostrarProductos = (productos, contenedor) => {
+    let botonesAgregar = document.querySelectorAll(".producto-agregar");
+
     // Limpiar el contenedor de productos antes de mostrar los nuevos productos
     contenedor.innerHTML = "";
 
@@ -60,21 +64,51 @@ const mostrarProductos = (productos, contenedor) => {
                 <h5 class="titulo-tarj">${producto.titulo}</h5>
                 <p class="tarj-descrip">${producto.descripcion}</p>
                 <p class="tarj-precio">$${producto.precio}</p>
-                <button class="btn-agregar">LO QUIERO!</button>
+                <p class="tarj-cant">${producto.cantidad}</p>
+                <button class="producto-agregar" id="${producto.id}">AGREGAR!</button>
             </div>
         `;
 
-        // Agregar el evento al botón
-        div.querySelector(".btn-agregar").addEventListener("click", () => {
-            agregarAlCarrito(producto);
-        });
-
-        // Agregar el div al contenedor de productos
+        //        Agregar el div al contenedor de productos
         contenedor.appendChild(div);
-    });
+   });
 };
+
+
+
+function actualizarBotonesAgregar () {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton=> {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+const productosEnCarrito =[];
+
+function agregarAlCarrito(e) {
+
+    const id = e.currentTarget.id;
+    console.log(id);
+
+}
+
+
+
+
+
+
+
+
+        // Agregar el evento al botón
+        // div.querySelector(".producto-agregar").addEventListener("click", () => {
+        //     agregarAlCarrito(producto);
+        // });
+
+
+
 
 // Función ficticia para agregar al carrito (puedes personalizarla según tu lógica)
-const agregarAlCarrito = (producto) => {
-    console.log(`Producto añadido: ${producto.titulo}`);
-};
+//const agregarAlCarrito = (producto) => {
+   // console.log(`Producto añadido: ${producto.titulo}`);
+//};
